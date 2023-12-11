34f489d3b5bfc3f904d6550f7ae2a683d5224853
7erz <leesncat@naver.com>
2023-12-11 19:45:41 +0900

    231211 UGUI 버튼

---
+++ UnityTutorial/Assets/Anim1/Scripts/DataManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public float[] times;
+
+    public void Start()
+    {
+        for(int i=0;i<times.Length;i++)
+        {
+            Debug.Log(times[i]);
+        }
+    }
+
+    
+
+}
